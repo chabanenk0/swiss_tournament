8e12ba16f873c78cb6fbfcc6e166a877bdfc4469
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-02-26 21:10:17 +0200

    Modified RounResultFixtures

--- src/DataFixtures/RoundResultFixtures.php
+++ src/DataFixtures/RoundResultFixtures.php
@@ -11,19 +11,19 @@ class RoundResultFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-//        $roundResult = new RoundResult();
-//        $roundResult->setTournament($this->getReference('Tournament1'));
-//        $roundResult->setStartTimestamp(0);
-//        $roundResult->setEndTimestamp(100);
-//        $roundResult->setBlackParticipant($this->getReference('Participant1'));
-//        $roundResult->setBlackTimeSpentInSeconds(20);
-//        $roundResult->setWhiteParticipant($this->getReference('Participant2'));
-//        $roundResult->setWhiteTimeSpentInSeconds(25);
-//        $roundResult->setFirstMove($this->getReference('FirstMove1'));
-//        $roundResult->setRound($this->getReference('Round'));
-//
-//        $manager->persist($roundResult);
-//        $manager->flush();
+        $roundResult = new RoundResult();
+        $roundResult->setTournament($this->getReference('Tournament1'));
+        $roundResult->setStartTimestamp(0);
+        $roundResult->setEndTimestamp(100);
+        $roundResult->setBlackParticipant($this->getReference('Participant0'));
+        $roundResult->setBlackTimeSpentInSeconds(20);
+        $roundResult->setWhiteParticipant($this->getReference('Participant1'));
+        $roundResult->setWhiteTimeSpentInSeconds(25);
+        $roundResult->setFirstMove($this->getReference('FirstMove1'));
+        $roundResult->setRound($this->getReference('Round'));
+
+        $manager->persist($roundResult);
+        $manager->flush();
     }
 
     public function getDependencies()
