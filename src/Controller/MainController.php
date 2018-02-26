3dba51990834aa877f4eaa6bd7a979b277a0e339
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-02-26 23:34:01 +0200

    Installed server

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
-    public function indexAction()
+    public function index()
     {
         return $this->render('');
     }
