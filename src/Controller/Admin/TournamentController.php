2aa72fc2fcb4beb4e2e6129891f35b6eb471c291
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-03-15 09:11:43 +0200

    Created list of players in players.html.twig

--- src/Controller/Admin/TournamentController.php
+++ src/Controller/Admin/TournamentController.php
@@ -2,13 +2,26 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Participant;
+use App\Entity\Player;
+use App\Entity\Tournament;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TournamentController extends Controller
 {
-    public function showPlayers()
+    public function showPlayers($title)
     {
-        return $this->render('admin/players.html.twig');
+        $players = $this->getDoctrine()->getRepository(Player::class)->findAll();
+        $tournament = $this->getDoctrine()->getRepository(Tournament::class)->findBy(['title' => $title]);
+        foreach ($tournament as $item) {
+            $tournamentId = $item->getId();
+        }
+        $participants = $this->getDoctrine()->getRepository(Participant::class)->findBy(['tournament' => $tournamentId]);
+
+        return $this->render('admin/players.html.twig', [
+            'participants' => $participants,
+            'players' => $players,
+        ]);
     }
 
 }
