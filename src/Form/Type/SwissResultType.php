bfd3a57958df92c4056b705a5286c033b6d610d6
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-04-27 09:56:17 +0300

    Used constants of results instead numbers in formtype

--- src/Form/Type/SwissResultType.php
+++ src/Form/Type/SwissResultType.php
@@ -2,25 +2,29 @@
 
 namespace App\Form\Type;
 
-use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use App\Repository\ParticipantRepository;
+use App\Entity\RoundResult;
 
 class SwissResultType extends AbstractType
 {
+    private $roundResult;
+
+    public function __construct()
+    {
+        $this->roundResult = new RoundResult();
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('result', ChoiceType::class, [
                 'choices' => [
-                    'Black win' => 'RESULT_BLACK_WIN',
-                    'Draw' => 'RESULT_DRAW',
-                    'White win' => 'RESULT_WHITE_WIN',
+                    'Black win' => $this->roundResult::RESULT_BLACK_WIN,
+                    'Draw' => $this->roundResult::RESULT_DRAW,
+                    'White win' => $this->roundResult::RESULT_WHITE_WIN,
                 ],
                 'label' => 'Round result',
             ])
@@ -29,3 +33,4 @@ class SwissResultType extends AbstractType
             ]);
     }
 }
+
